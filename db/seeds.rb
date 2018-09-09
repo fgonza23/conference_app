9ec9fb87c4858776f274b221106c9b833def7a36
fgonza23 <fgonza0267@gmail.com>
2018-09-08 23:37:32 -0500

    updated conference_app

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,18 @@ speaker = Speaker.create(
               {first_name: first_name,
                last_name: last_name,
                 email: Faker::Internet.free_email("#{first_name}.#{last_name}")}
+                
+    
                 )
 
 speaker.save
 
 end
+
+meeting = Meeting.create(
+              {title: "BookClub",
+                agenda: "Meeting to discuss weekly books read",
+                time: "2:00 PM"}
+                )
+
+meeting.save
